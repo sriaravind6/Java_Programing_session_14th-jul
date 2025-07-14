3e48f13260b2c664e28216d48809058710f78471
sriaravind6 <aravindeee218@gmail.com>
2025-07-14 10:11:29 +0530

    Day 1 Class Notes

--- daily_class_for_java/First_Day_JavaClass.java
+++ daily_class_for_java/First_Day_JavaClass.java
@@ -1,4 +1,24 @@
 package daily_class_for_java;
 
 public class First_Day_JavaClass {
+    public void method1() {
+        System.out.println("This is method1"); // set of actions
+    }
+    private void method2() {
+        System.out.println("This is method2"); // set of actions
+    }
+    private void method3() {
+        System.out.println("This is method3"); // set of actions
+    }
+    public static void main(String[] args){
+        // class reference obj = new class reference();
+        // Creating an object of the class
+        //new is the keyword to create an object
+        First_Day_JavaClass class1Obj = new First_Day_JavaClass();
+        // Calling methods using the object
+        class1Obj.method1();
+        class1Obj.method2();
+        class1Obj.method3();
+    }
+
 }
---
+++ daily_class_for_java/First_Day_JavaClass2.java
@@ -0,0 +1,9 @@
+package daily_class_for_java;
+
+public class First_Day_JavaClass2 {
+    public static void main(String[] args){
+        First_Day_JavaClass class1Obj = new First_Day_JavaClass();
+        class1Obj.method1();
+    }
+
+}
