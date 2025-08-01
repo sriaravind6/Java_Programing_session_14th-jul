e4c42b7f97031c57b8bdda9e8caa3e2766181877
Aravind <aravindeee218@gmail.com>
2025-08-01 13:10:56 +0530

    looping concepts

---
+++ daily_class_for_java/ninthclass/LoopsConcepts.java
@@ -0,0 +1,58 @@
+package daily_class_for_java.ninthclass;
+
+import java.util.HashMap;
+
+public class LoopsConcepts {
+    public static void main(String[] args) {
+//        for (int i = 0; i <20 ; i++) {
+//            System.out.println("My name is Aravind");
+//        }
+//        int i=20;
+//        do{
+//            System.out.println("Given number more than 20 is: ");
+//            System.out.println("My name is Aravind");
+//            i++;
+//        }
+//        while (i <20);
+
+//        while(i < 30) {
+//            System.out.println("Given number less than 30 is: ");
+//            System.out.println("My name is Aravind");
+//            i++;
+//        }
+
+        //1 2 3 4 5
+        //1 2 3 4 5
+        //1 2 3 4 5
+        //1 2 3 4 5
+        //1 2 3 4 5
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 1; j <=5 ; j++) {
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//        }
+//        int i = 1; // Initialize i to 0
+//        while(i<5 && i >= 0) { // Loop while i is less than 5 and non-negative
+//            for (int j = 1; j <=5 ; j++) {
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//            --i;
+//        }
+
+
+//        String name = "aravind";
+//        HashMap<Character,Integer> charCountMap = new HashMap<>();
+//        for (int j = 0; j < name.length(); j++) {
+//            char x = name.charAt(j);
+//            if(!charCountMap.containsKey(x)) {
+//                charCountMap.put(x, 0);
+//            }
+//            charCountMap.put(x,charCountMap.get(x)+1);
+//        }
+//        System.out.println("Character count map: " + charCountMap);
+
+
+    }
+}
