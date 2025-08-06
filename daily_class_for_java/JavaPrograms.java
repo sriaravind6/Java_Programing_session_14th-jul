78ff4a69ca2302daaddfe6ad0065addb921cfe14
Aravind <aravindeee218@gmail.com>
2025-08-06 22:03:40 +0530

    Programmes

---
+++ daily_class_for_java/JavaPrograms.java
@@ -0,0 +1,67 @@
+package daily_class_for_java;
+
+public class JavaPrograms {
+
+    public static void main(String[] args) {
+//        int n = 50;
+//        if (n % 2 == 0) System.out.println("Even");
+//        else System.out.println("Odd");
+//        int evenCount = 0,oddCount = 0;
+//        for (int i = 1; i <= 50; i++) {
+//            if (i % 2 == 0) evenCount++;
+//            else oddCount++;
+//        }
+//        System.out.println("Even Count: " + evenCount);
+//        System.out.println("Odd Count: " + oddCount);
+//        factorial(5); 1X2X3X4X5;
+//        int factorial = 1;
+//        for (int i = 1; i <=5; i++) {
+//            factorial *= i; // factorial = factorial * i;
+//        }
+//        System.out.println(factorial);
+
+        // n = 5; 1X5 5X1
+//        37 1X37   37X1
+//        int n = 41;
+//        int count=0;
+//        for (int i = 1; i <=n ; i++) {
+//            if( n % i == 0) {
+//                count++;
+//            }
+//        }
+//        if (count == 2) {
+//            System.out.println(n + " is a prime number");
+//        } else {
+//            System.out.println(n + " is not a prime number");
+//        }
+
+        // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+// 0 1 1 2 3 5 8 13 21 34 55 89
+//        int n = 10 ;
+//        int a = 0, b = 1, c;
+//        System.out.print(a + " " + b + " ");
+//        for (int i = 2; i <= n; i++) {
+//            c = a + b; // c = 0 + 1 = 1
+//            System.out.print(c + " ");
+//            a = b; // a = 1
+//            b = c; // b = 1
+//        }
+
+        // 123 ====> 321
+//        int n = 123;
+//        int m = n;
+//        int reverse = 0; //3   10+2 =12  120+1 = 121
+//        while (n != 0) {
+//            reverse = reverse * 10 + n % 10;
+//            n = n / 10; // n = 12
+//        }
+//        System.out.println("Reverse of the number is: " + reverse);
+//
+//        if (reverse == m) {
+//            System.out.println(m + " is a palindrome number");
+//        } else {
+//            System.out.println(m + " is not a palindrome number");
+//        }
+    }
+
+}
